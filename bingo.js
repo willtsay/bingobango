19b741f49c87990d82c6f3f821df561a3fbb93cf
Will <willtsay@gmail.com>
2022-08-03 22:54:26 -0700

    basic table editting

--- bingo.js
+++ bingo.js
@@ -3,10 +3,25 @@ var bingo = document.getElementById("bingoTab");
 var bingoThingies;
 
 function getTableCell(i, j){
-	var cell = bingo.rows[i].cells()
-
+	console.log(bingo.rows[0].cells[0].innerHTML);
+	var cell = bingo.rows[i].cells[j];
+	bingo.rows[0].cells[0].innerHTML = "asdf";
+	console.log(bingo.rows[0].cells[0].innerHTML);
+	console.log('yolo');
 }
 
-function setTableCell(i, j){
+function setTableCell(i, j, text){
 
+	bingo.rows[i].cells[j].innerHTML = text;
+	console.log(text);
 }
+
+
+getTableCell(2,2);
+
+
+
+window.onload = (event) => {
+  console.log('page is fully loaded');
+  setTableCell(4,4, "freemoney")
+};
